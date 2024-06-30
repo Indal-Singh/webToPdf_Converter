48ef94728ec111c3c52d8b84863efa62d46fca8b
Nikusoft <nikusoft.jsr@gmail.com>
2024-06-30 08:38:41 +0530

    vercel setings update

---
+++ install_dependencies.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y \
+  gconf-service \
+  libasound2 \
+  libatk1.0-0 \
+  libcups2 \
+  libdbus-1-3 \
+  libfontconfig1 \
+  libgbm1 \
+  libgconf-2-4 \
+  libgtk-3-0 \
+  libnspr4 \
+  libnss3 \
+  libxss1 \
+  lsb-release \
+  xdg-utils \
+  wget
